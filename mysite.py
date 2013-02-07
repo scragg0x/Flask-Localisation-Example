c76d3de24d12829f7d7bc0f385d0232b4d1d4bca
Matthew <scragg@gmail.com>
2013-02-06 22:39:33 -0600

    init

---
+++ mysite.py
@@ -0,0 +1,41 @@
+from flask import Flask, Blueprint, g, redirect, request
+
+app = Flask(__name__)
+
+mod = Blueprint('mysite', __name__, url_prefix='/<lang_code>')
+
+sites = {
+    'mysite.com': 'en',
+    'myothersite.com': 'fr'
+}
+
+@app.url_defaults
+def add_language_code(endpoint, values):
+    values.setdefault('lang_code', g.lang_code)
+
+@app.url_value_preprocessor
+def pull_lang_code(endpoint, values):
+    url = request.url.split('/', 3)
+    g.lang_code = sites[url[2]]
+
+@mod.url_defaults
+def add_language_code(endpoint, values):
+    values.setdefault('lang_code', g.lang_code)
+
+@mod.url_value_preprocessor
+def pull_lang_code(endpoint, values):
+    g.lang_code = values.pop('lang_code')
+    url = request.url.split('/', 3)
+    if not g.lang_code:
+        g.lang_code = sites[url[2]]
+
+@mod.route('/')
+def index():
+    # Use g.lang_code to pull localized data for template
+    return 'lang = %s' % g.lang_code
+
+@app.route('/')
+def index():
+    return 'lang = %s' % g.lang_code
+
+app.register_blueprint(mod)
---
+++ tests.py
@@ -0,0 +1,62 @@
+import os
+import unittest
+import re
+import requests
+import urllib2
+import json
+from mysite import app
+
+class MySiteTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SERVER_NAME'] = 'mysite.com'
+        self.domain = 'http://mysite.com/'
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_en_index(self):
+        rv = self.app.get('/en/', self.domain)
+        self.assertEqual(rv.data, 'lang = en')
+        print self.domain, rv.data
+
+    def test_fr_index(self):
+        rv = self.app.get('/fr/', self.domain)
+        self.assertEqual(rv.data, 'lang = fr')
+        print self.domain, rv.data
+
+    def test_default(self):
+        rv = self.app.get('/', self.domain)
+        self.assertEqual(rv.data, 'lang = en')
+        print self.domain, rv.data
+
+class MyOtherSiteTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SERVER_NAME'] = 'myothersite.com'
+        self.domain = 'http://myothersite.com/'
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_en_index(self):
+        rv = self.app.get('/en/', self.domain)
+        self.assertEqual(rv.data, 'lang = en')
+        print self.domain, rv.data
+
+    def test_fr_index(self):
+        rv = self.app.get('/fr/', self.domain)
+        self.assertEqual(rv.data, 'lang = fr')
+        print self.domain, rv.data
+
+    def test_default(self):
+        rv = self.app.get('/', self.domain)
+        self.assertEqual(rv.data, 'lang = fr')
+        print self.domain, rv.data
+
+if __name__ == '__main__':
+    unittest.main()
